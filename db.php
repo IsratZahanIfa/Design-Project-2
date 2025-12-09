3e2e03b3e721e02c0c51c8b5081b0d54556afe7b
Israt Zahan Ifa <israt.zahan.cse@ulab.edu.bd>
2025-12-10 02:26:56 +0600

    n/c

--- db.php
+++ db.php
@@ -1,12 +1,17 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$dbname = "agrotradehub";
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
-$conn = mysqli_connect($host, $user, $pass, $dbname);
+$host = getenv("b.fr-paril.bengt.wasmernet.com");
+$port = getenv("10272");
+$user = getenv("88377ea378be800013b448d66296");
+$pass = getenv("06938837-7ea3-7a9e-8000-805880cedec7");
+$db   = getenv("agrotradehub");
+
+$conn = mysqli_connect($host, $user, $pass, $db, $port);
 
 if (!$conn) {
-    die("Database Connection Failed: " . mysqli_connect_error());
+    die("Connection failed: " . mysqli_connect_error());
 }
 ?>
